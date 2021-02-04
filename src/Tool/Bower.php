6561c4bfe651888b144c1e60c6caeb307bc1dac0
Michael Gauthier <mike@silverorange.com>
2021-02-04 19:43:38 -0400

    Add n support and use n to run node-based tools if project specifies an engine

--- src/Tool/Bower.php
+++ src/Tool/Bower.php
@@ -8,16 +8,18 @@ use Silverorange\PackageRelease\Console\ProcessRunner;
 /**
  * @package   PackageRelease
  * @author    Michael Gauthier <mike@silverorange.com>
- * @copyright 2018 silverorange
+ * @copyright 2018-2021 silverorange
  * @license   http://www.opensource.org/licenses/mit-license.html MIT License
  */
 class Bower
 {
     public static function install(OutputInterface $output): bool
     {
+        $prefix = N::getPrefix($output);
+
         return (new ProcessRunner(
             $output,
-            'bower install',
+            $prefix . 'bower install',
             'installing bower dependencies',
             'installed bower dependencies',
             'failed to install bower dependencies'
---
+++ src/Tool/N.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Silverorange\PackageRelease\Tool;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use Silverorange\PackageRelease\Console\ProcessRunner;
+
+/**
+ * @package   PackageRelease
+ * @author    Michael Gauthier <mike@silverorange.com>
+ * @copyright 2021 silverorange
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+class N
+{
+    private static $isAppropriate = null;
+    private static $isDownloaded = false;
+
+    public static function download(OutputInterface $output): bool
+    {
+        if (self::isAppropriate() && !self::$isDownloaded) {
+            return (new ProcessRunner(
+                $output,
+                'n --download engine',
+                'downloading required node version',
+                'downloaded required node version',
+                'failed to download required node version'
+            ))->run();
+
+            self::$isDownloaded = true;
+        }
+    }
+
+    public static function getPrefix(OutputInterface $output): string
+    {
+        self::download($output);
+
+        if (self::isAppropriate()) {
+            return 'n exec engine ';
+        }
+
+        return '';
+    }
+
+    public function isAppropriate(): bool
+    {
+        if (self::$isAppropriate === null) {
+            if ($this->hasFile('package.json')) {
+                $json = json_decode(file_get_contents('package.json'), true);
+                self::$isAppropriate =  (
+                    isset($json['engines']) && isset($json['engines']['node'])
+                );
+            } else {
+                self::$isAppropriate = false;
+            }
+        }
+
+        return self::$isAppropriate;
+    }
+
+    protected function hasFile(string $filename): bool
+    {
+        return (file_exists($filename) && is_readable($filename));
+    }
+}
