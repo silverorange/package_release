d13ff6321c3d065e464247e000e4f9f5ce0cbc98
pparke <pparke@users.noreply.github.com>
2021-03-25 11:14:52 -0300

    Only use gulp when node < 14.15.1
    Checks if the repo has a package.json file and checks that file for an engines.node field that is greater than or equal to 14.15.1

--- src/Builder/LegacyPHPBuilder.php
+++ src/Builder/LegacyPHPBuilder.php
@@ -16,7 +16,16 @@ class LegacyPHPBuilder extends BaseBuilder
 {
     public function isAppropriate(): bool
     {
-        return $this->hasFile('composer.json');
+        $has_composer_json = $this->hasFile('composer.json');
+        $node_14_15_1 = false;
+        if ($this->hasFile('package.json')) {
+            $json = json_decode(file_get_contents('package.json'));
+            if (property_exists($json, 'engines') && property_exists($json->engines, 'node')) {
+                $node_14_15_1 =  version_compare($json->engines->node, "14.15.1", ">=");
+            }
+        }
+        
+        return $has_composer_json && !$node_14_15_1;
     }
 
     public function build(OutputInterface $output): bool
