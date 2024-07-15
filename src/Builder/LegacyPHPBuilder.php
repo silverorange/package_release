a2a70a10202dbcfbd2f2035ca110fd70e2bed657
Michael Gauthier <mike@silverorange.com>
2024-07-15 15:37:46 -0300

    Use Npm tool to run legaseer concentrate script
    This allows it to work with npm, pnpm, or yarn.

--- src/Builder/LegacyPHPBuilder.php
+++ src/Builder/LegacyPHPBuilder.php
@@ -5,13 +5,13 @@ namespace Silverorange\PackageRelease\Builder;
 use Symfony\Component\Console\Output\OutputInterface;
 use Silverorange\PackageRelease\Tool\Composer;
 use Silverorange\PackageRelease\Tool\Gulp;
-use Silverorange\PackageRelease\Tool\Legaseer;
+use Silverorange\PackageRelease\Tool\Npm;
 use Silverorange\PackageRelease\Tool\PackageJson;
 
 /**
  * @package   PackageRelease
  * @author    Michael Gauthier <mike@silverorange.com>
- * @copyright 2018 silverorange
+ * @copyright 2018-2024 silverorange
  * @license   http://www.opensource.org/licenses/mit-license.html MIT License
  */
 class LegacyPHPBuilder extends BaseBuilder
@@ -29,7 +29,7 @@ class LegacyPHPBuilder extends BaseBuilder
         $result = Composer::install($output);
 
         $result = $result && (!$hasSoGulp || Gulp::concentrate($output));
-        $result = $result && (!$hasLegaseer || Legaseer::concentrate($output));
+        $result = $result && (!$hasLegaseer || Npm::run($output, 'concentrate'));
 
         return $result;
     }
